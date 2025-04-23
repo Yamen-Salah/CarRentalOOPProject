06109252781dfa972b7709233c20b7124d9ed162
Yamen-Salah <yamensalah27@gmail.com>
2025-04-23 12:52:09 -0400

    Create seperate class for users (Users.java)

---
+++ User.java
@@ -0,0 +1,13 @@
+public class User {
+    private int id;
+    private String name;
+    private String phone;
+
+    public User(int id, String name, String phone) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+    }
+
+    // Getters and setters will come later
+}
