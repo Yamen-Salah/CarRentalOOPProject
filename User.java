6bffbb598609fc687a81612c22dc845c40ba0d95
Yamen-Salah <yamensalah27@gmail.com>
2025-04-24 12:19:15 -0400

    Add Getters and constructors

--- User.java
+++ User.java
@@ -10,4 +10,22 @@ public class User {
     }
 
     // Getters and setters will come later
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
